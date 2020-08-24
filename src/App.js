3022a266799286f825a1e34f9f58cfb0daa0e59b
Moses <lazaromoses77@gmail.com>
2020-08-24 15:02:23 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from "./Components/Home"
+import 'leaflet/dist/leaflet.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
